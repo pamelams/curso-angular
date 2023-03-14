720d4c87d06d23da93e0d13ab3a3840e696bb28e
pamelams <pms2@ic.ufal.br>
2023-03-14 16:50:53 -0300

    [NINC-1235][NINC-2005] Create Users Service

--- services-assignment-start/src/app/active-users/active-users.component.ts
+++ services-assignment-start/src/app/active-users/active-users.component.ts
@@ -1,15 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
+export class ActiveUsersComponent implements OnInit{
+  @Input() users!: string[];
   @Output() userSetToInactive = new EventEmitter<number>();
 
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.usersService.activeUsers;
+  }
+
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.usersService.setToInactive(id);
   }
 }
