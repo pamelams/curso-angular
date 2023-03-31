0303518dfd159ce8302b4107cc9986ce9961ae22
pamelams <pms2@ic.ufal.br>
2023-03-31 15:28:57 -0300

    [NINC-1235][NINC-2016] Controlling Navigation with canDeactivate

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactive-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
     //canActivate: [AuthGuard], 
     canActivateChild: [AuthGuard],
     component: ServersComponent, children: [
-      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
       { path: ':id', component: ServerComponent },
     ] },
     { path: 'not-found', component: PageNotFoundComponent },
