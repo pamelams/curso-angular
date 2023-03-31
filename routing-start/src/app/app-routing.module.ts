e40ca2788f1f1de4ce14da5f7f387954fb21e471
pamelams <pms2@ic.ufal.br>
2023-03-31 16:50:42 -0300

    [NINC-1235][NINC-2016] Resolving Dynamic Data with the resolve Guard

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactive-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
@@ -20,8 +21,8 @@ const appRoutes: Routes = [
     //canActivate: [AuthGuard], 
     canActivateChild: [AuthGuard],
     component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
-      { path: ':id', component: ServerComponent },
     ] },
     //{ path: 'not-found', component: PageNotFoundComponent },
     { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
