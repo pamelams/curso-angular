464252a25a333952ebde48d638df100f427f6fc8
pamelams <pms2@ic.ufal.br>
2023-03-31 16:17:57 -0300

    [NINC-1235][NINC-2016] Controlling Navigation with canDeactivate

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -28,16 +28,24 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
       }
     );
     this.route.fragment.subscribe();
-    const id = this.route.snapshot.params['id']
+    let id = +this.route.snapshot.params['id']
     this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    this.route.params.subscribe(
+      (params: Params) => {
+        id = +params['id'];
+        this.server = this.serversService.getServer(id);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+      } 
+    );
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
     this.changesSaved = true;
-    this.router.navigate(['../'], {relativeTo: this.route});
+    this.router.navigate(['../'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
