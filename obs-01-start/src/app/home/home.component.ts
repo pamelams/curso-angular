343b44e554a7dc7b5ff022e000823ceb7606293d
pamelams <pms2@ic.ufal.br>
2023-04-10 17:49:20 -0300

    [NINC-1235][NINC-1991] Building a Custom Observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs'
+import { Observable, Subscription, interval } from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -12,10 +12,20 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+    const customIntervalObs = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+    
+    this.firstObsSubscription = customIntervalObs.subscribe(data => {
+      console.log(data);
     })
-
   }
 
   ngOnDestroy(): void {
