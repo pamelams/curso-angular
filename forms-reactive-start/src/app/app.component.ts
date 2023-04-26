bde4fef965b9370c4e6d7f6c07d1fc95e8588ad7
pamelams <pms2@ic.ufal.br>
2023-04-26 12:17:28 -0300

    [NINC-1235][NINC-2005] Creating Custom Validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), // bind(this) passa o this atual para a função
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -33,4 +34,17 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null; // quando válido devemos passar nada, ou null
+  }
+  // forbiddenNames = (control: FormControl): {[s: string]: boolean} => { // usando arrow function o bind(this) nãoé necessário
+  //   if(this.forbiddenUsernames.indexOf(control.value)) {
+  //     return {'nameIsForbidden': true};
+  //   }
+  //   return null; 
+  // }
 }
