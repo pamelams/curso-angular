622f894b21cc8fc272e61658bc75301ab0bdb6d0
pamelams <pms2@ic.ufal.br>
2023-04-27 10:36:06 -0300

    [NINC-1235][NINC-2005] Add assignment 7

---
+++ tarefa-7/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'tarefa-7';
+  signupForm!: FormGroup;
+  status = ['stable', 'critical', 'finished'];
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'projectData': new FormGroup({
+        'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'projectStatus': new FormControl('stable', {nonNullable: true}),
+      }),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+    this.signupForm.reset();
+
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} | null {
+    if(control.value === 'Test') {
+      return {'nameisForbidden': true};
+    }
+    return null;
+  }
+}
