04ce3a3f2f0001fcf87955cf38d9deb63886b645
pamelams <pms2@ic.ufal.br>
2023-05-02 17:20:24 -0300

    [NINC-1235][NINC-1984] Loading the Shopping List Items into the Form

--- course-project/src/app/shared/services/shopping-list.service.ts
+++ course-project/src/app/shared/services/shopping-list.service.ts
@@ -9,15 +9,22 @@ export class ShoppingListService {
         new Ingredient('Tomatoes', 8)
     ];
     ingredientsChanged = new Subject<void>();
+    startedEditing = new Subject<number>();
     
     getIngredients() {
         return this.ingredients.slice();
     }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredientName: string, ingredientAmount: number) {
         const newIngredient = new Ingredient(ingredientName, ingredientAmount);
         this.ingredients.push(newIngredient);
         this.ingredientsChanged.next();
     }
+
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next();
--- course-project/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,20 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ShoppingListService } from 'src/app/shared/services/shopping-list.service';
 import { Ingredient } from '../../shared/ingredient.model';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent {
+export class ShoppingListEditComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
+  editMode = false;
+  editedItemIndex!: number;
+  editedItem!: Ingredient;
+  @ViewChild('f') shopListForm!: NgForm;
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing
+    .subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.shoppingListService.getIngredient(index);
+        this.shopListForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        });
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   onAddIngredient(form: NgForm) {
     const value = form.value;
     this.shoppingListService.addIngredient(value.name, value.amount);
+  }
+
+  onDelete() {
+
+  }
+
+  onClear() {
+
   }
 
 }
