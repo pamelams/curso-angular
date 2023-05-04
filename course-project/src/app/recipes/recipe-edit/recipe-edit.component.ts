889cab9a8b5126f942ef9d6533708686e339d495
pamelams <pms2@ic.ufal.br>
2023-05-04 10:47:59 -0300

    [NINC-1235][NINC-1984] Creating the form for editing recipes

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipesService } from 'src/app/shared/services/recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -14,22 +15,28 @@ export class RecipeEditComponent implements OnInit {
   recipeForm!: FormGroup;
   ingredients: {name: string, amount: number}[] = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipesService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = + params['id'];
         this.editMode = params['id'] != null;
-        console.log(this.editMode);
+        this.initForm();
       }
     );
+    
+    
+  }
+
+  private initForm() {
     this.recipeForm = new FormGroup({
-      'name': new FormControl(null, Validators.required),
-      'imagePath': new FormControl(null),
-      'description': new FormControl(null, Validators.required),
+      'name': new FormControl((this.editMode ? this.recipeService.getRecipe(this.id).name : null), Validators.required),
+      'imagePath': new FormControl((this.editMode ? this.recipeService.getRecipe(this.id).imagePath : null)),
+      'description': new FormControl((this.editMode ? this.recipeService.getRecipe(this.id).description : null), Validators.required),
       'ingredients': new FormArray([]),
     });
+
   }
 
   onSubmit() {
