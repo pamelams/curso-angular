2ed7573caa0671688e9e170dd03d345deeb1a4f0
pamelams <pms2@ic.ufal.br>
2023-05-16 11:00:25 -0300

    [NINC-1235][NINC-1993] Parametrizing a Custom Pipe

--- pipes-start/src/app/shorten.pipe.ts
+++ pipes-start/src/app/shorten.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({name: 'shorten'})
 export class ShortenPipe implements PipeTransform {
-    transform(value: any) {
-        return (value.substr(0, 10) + (value.substr(0, 10) === value.substr(0, ) ? '' : '...'));
+    transform(value: any, limit: number = 10) {
+        return (value.substr(0, limit) + (value.substr(0, limit) === value.substr(0, ) ? '' : '...'));
     }
 }
