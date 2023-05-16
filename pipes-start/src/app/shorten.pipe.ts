483abdaaf7206bee3cd7289343397110f94abf6f
pamelams <pms2@ic.ufal.br>
2023-05-16 10:56:51 -0300

    [NINC-1235][NINC-1993] Creating a Custom Pipe

---
+++ pipes-start/src/app/shorten.pipe.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({name: 'shorten'})
+export class ShortenPipe implements PipeTransform {
+    transform(value: any) {
+        return (value.substr(0, 10) + (value.substr(0, 10) === value.substr(0, ) ? '' : '...'));
+    }
+}
