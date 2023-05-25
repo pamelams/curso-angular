e3798b855c454a9124541a2ac4eafa61bacad1bc
pamelams <pms2@ic.ufal.br>
2023-05-25 18:00:49 -0300

    [NINC-1235][NINC-2016] Using a Service for Http Requests

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,46 +13,25 @@ export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    this.http.post<{name: string}>(
-      'https://http-01-backend-default-rtdb.firebaseio.com/posts.json', 
-      postData
-      ).subscribe(responseData => {
-        console.log(responseData);
-    });
+    this.postsService.createAndStorePost(postData.title, postData.content);    
   }
 
   onFetchPosts() {
-    // Send Http request
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http.get<{[key: string]: Post}>(
-      'https://http-01-backend-default-rtdb.firebaseio.com/posts.json'
-      ).pipe(map((responseData) => {
-        const postsArray: Post[] = [];
-        for(const key in responseData) {
-          if(responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key], id: key })
-          }
-        }
-        return postsArray;
-      })).subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-    });
   }
 }
