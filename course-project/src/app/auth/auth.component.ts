4bb1a6198ab25e35b43f73df17992e2fb7b05e2e
pamelams <pms2@ic.ufal.br>
2023-06-05 11:23:13 -0300

    [NINC-1235][NINC-1991] Sending the Signup Request

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -9,12 +10,29 @@ import { NgForm } from "@angular/forms";
 export class AuthComponent {
     isLoginMode = true;
 
+    constructor(private authService: AuthService) {}
+
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
 
     }
 
     onSubmit(form: NgForm) {
-        console.log(form);
+        if(!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+        if(this.isLoginMode) {
+            // ...
+        }
+        else {
+            this.authService.signUp(email, password).subscribe(respData => {
+                console.log(respData);
+            }, error => {
+                console.log(error);
+            });
+        }        
+        form.reset();
     }
 }
