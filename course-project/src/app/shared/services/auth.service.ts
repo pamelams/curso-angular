a1657bb3e6e84211a9eeefbb93403a909f4e2659
pamelams <pms2@ic.ufal.br>
2023-06-14 12:26:17 -0300

    [NINC-1235][NINC-1991]  Adding Auto-Logout

--- course-project/src/app/shared/services/auth.service.ts
+++ course-project/src/app/shared/services/auth.service.ts
@@ -17,7 +17,8 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
     user = new BehaviorSubject<User | null>(null);
-    
+    private tokenExpirationTimer: any;
+
     constructor(private http: HttpClient, private router: Router) {}
 
     signUp(email: string, password: string) {
@@ -61,12 +62,25 @@ export class AuthService {
 
         if (loadedUser.token) {
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
         }
     }
 
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+    
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
@@ -74,6 +88,7 @@ export class AuthService {
         const user = new User(email, userId, token, expirationDate);
         console.log(user);
         this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 
