c3fba882ed30d6efebfcc37f64291018b4ff9a50
pamelams <pms2@ic.ufal.br>
2023-07-11 18:23:27 -0300

    [NINC-1235][NINC-1993] Lazy loading with standalone components

---
+++ 03-starting-with-routing/src/app/dashboard/dashboard-routes.ts
@@ -0,0 +1,13 @@
+import { Route } from "@angular/router";
+import { DashboardComponent } from "./dashboard.component";
+import { TodayComponent } from "./today/today.component";
+
+export const DASHBOARD_ROUTES: Route[] = [
+    {
+        path: '',
+        component: DashboardComponent
+    }, {
+        path: 'today',
+        component: TodayComponent
+    }
+]
