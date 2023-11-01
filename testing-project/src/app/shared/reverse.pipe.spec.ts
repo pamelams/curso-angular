b5e6b2617c280d0cae3a6b8016632b3ea8ef7866
pamelams <pms2@ic.ufal.br>
2023-11-01 10:53:00 -0300

    [NINC-1235][NINC-1984] Add Unit Testing project

---
+++ testing-project/src/app/shared/reverse.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { ReversePipe } from "./reverse.pipe";
+
+describe('Pipe: ReversePipe', () => {
+  it('should reverse the inputs', () => {
+    let reversePipe = new ReversePipe();
+    expect(reversePipe.transform('hello')).toEqual('olleh');
+  });
+
+});
